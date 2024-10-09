721e8722d6bd36ada440db880bb8c1723f52202e
zhekichan <evg.ermolov@yandex.ru>
2024-10-09 11:35:21 +0500

    navigation fix

--- app/src/main/java/com/example/gridpics/ui/pictures/PicturesFragment.kt
+++ app/src/main/java/com/example/gridpics/ui/pictures/PicturesFragment.kt
@@ -12,7 +12,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
