61eeb2c69c598760881c2a2ccdc485c3520a56e0
zhekichan <evg.ermolov@yandex.ru>
2024-10-10 14:28:15 +0500

    work with zoom DetailsFragment.kt

--- app/src/main/java/com/example/gridpics/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/gridpics/ui/details/DetailsFragment.kt
@@ -10,8 +10,6 @@ import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.WindowManager
 import android.widget.LinearLayout
-import android.widget.RelativeLayout
-import androidx.core.view.marginTop
 import androidx.fragment.app.Fragment
 import com.example.gridpics.R
 import com.example.gridpics.databinding.FragmentDetailsBinding
@@ -68,7 +66,6 @@ class DetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         var isVisible = true
-        val height = binding.holder.height
         val pic = binding.photoView
         binding.photoView.setOnClickListener {
             if (isVisible) {
@@ -81,9 +78,10 @@ class DetailsFragment : Fragment() {
                             View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 binding.backIcon.visibility = View.GONE
                 binding.url.visibility = View.GONE
-                val param = pic.layoutParams as ViewGroup.MarginLayoutParams
-                param.setMargins(0,70,0,0)
-                pic.layoutParams = param
+                //val param = pic.layoutParams as ViewGroup.MarginLayoutParams
+                //param.setMargins(0, 70, 0, 0)
+                //pic.layoutParams = param
+                pic.setPadding(0, 70, 0, 0)
                 Log.d("PicZOOM", "${pic.isZoomed}")
                 requireActivity().window.navigationBarColor = Color.TRANSPARENT
 
@@ -93,9 +91,10 @@ class DetailsFragment : Fragment() {
                 requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
                 binding.backIcon.visibility = View.VISIBLE
                 binding.url.visibility = View.VISIBLE
-                val param = pic.layoutParams as ViewGroup.MarginLayoutParams
-                param.setMargins(0,0,0,0)
-                pic.layoutParams = param
+                //val param = pic.layoutParams as ViewGroup.MarginLayoutParams
+                //param.setMargins(0, 0, 0, 0)
+                //pic.layoutParams = param
+                pic.setPadding(0, 0, 0, 0)
                 requireActivity().window.navigationBarColor = Color.BLACK
                 requireActivity().window.decorView.systemUiVisibility =
                     View.SYSTEM_UI_FLAG_VISIBLE or
