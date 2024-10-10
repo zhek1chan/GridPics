1bd595b616a0e91cacb1f0a525dea63242b12750
zhekichan <evg.ermolov@yandex.ru>
2024-10-10 14:14:00 +0500

    work with zoom DetailsFragment.kt

--- app/src/main/java/com/example/gridpics/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/gridpics/ui/details/DetailsFragment.kt
@@ -1,10 +1,8 @@
 package com.example.gridpics.ui.details
 
-import android.content.res.Configuration
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +10,8 @@ import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.WindowManager
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import androidx.core.view.marginTop
 import androidx.fragment.app.Fragment
 import com.example.gridpics.R
 import com.example.gridpics.databinding.FragmentDetailsBinding
@@ -55,61 +55,58 @@ class DetailsFragment : Fragment() {
             }
         })
 
-        val displayMetrics = DisplayMetrics()
-        requireActivity().windowManager.defaultDisplay.getMetrics(displayMetrics)
-        val picHeight = pic.drawable.minimumHeight
-        val picWidth = pic.drawable.minimumWidth
-        val k: Float
-        val dWidth: Int = displayMetrics.widthPixels
-        val orientation = this.resources.configuration.orientation
-        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            k = dWidth.toFloat() / picWidth.toFloat()
-            val imgHeight = (k * picHeight).toInt()
-            binding.photoView.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
-        } else {
-            k = dWidth.toFloat() / picHeight.toFloat()
-            val imgHeight = (k * picHeight).toInt()
-            binding.photoView.layoutParams = LinearLayout.LayoutParams(imgHeight, MATCH_PARENT)
-        }
+        pic.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+
 
         binding.backIcon.setOnClickListener {
             navigateBack()
         }
         binding.url.text = img
+        return root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         var isVisible = true
+        val height = binding.holder.height
+        val pic = binding.photoView
         binding.photoView.setOnClickListener {
             if (isVisible) {
                 requireActivity().window.setFlags(
                     WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 )
-                requireActivity().window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                requireActivity().window.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 binding.backIcon.visibility = View.GONE
                 binding.url.visibility = View.GONE
+                val param = pic.layoutParams as ViewGroup.MarginLayoutParams
+                param.setMargins(0,70,0,0)
+                pic.layoutParams = param
                 Log.d("PicZOOM", "${pic.isZoomed}")
                 requireActivity().window.navigationBarColor = Color.TRANSPARENT
+
                 isVisible = false
             } else {
                 isVisible = true
                 requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
                 binding.backIcon.visibility = View.VISIBLE
                 binding.url.visibility = View.VISIBLE
+                val param = pic.layoutParams as ViewGroup.MarginLayoutParams
+                param.setMargins(0,0,0,0)
+                pic.layoutParams = param
                 requireActivity().window.navigationBarColor = Color.BLACK
-                requireActivity().window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE or
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                requireActivity().window.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_VISIBLE or
+                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
             }
         }
-
-
 
         binding.layout.setOnClickListener {
             binding.backIcon.visibility = View.VISIBLE
             binding.url.visibility = View.VISIBLE
         }
-
-        return root
     }
 
     private fun navigateBack() {
