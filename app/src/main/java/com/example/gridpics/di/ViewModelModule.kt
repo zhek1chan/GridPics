b18cfd6796410448dc15bbe446ad73450d7b4e4b
zhekichan <evg.ermolov@yandex.ru>
2024-10-11 18:26:26 +0500

    migration on compose

--- app/src/main/java/com/example/gridpics/di/ViewModelModule.kt
+++ app/src/main/java/com/example/gridpics/di/ViewModelModule.kt
@@ -1,6 +1,7 @@
 package com.example.gridpics.di
 
 import com.example.gridpics.ui.pictures.PicturesViewModel
+import com.example.gridpics.ui.settings.SettingsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -8,6 +9,10 @@ val viewModelModule = module {
 
     viewModel {
         PicturesViewModel(get())
+    }
+
+    viewModel {
+        SettingsViewModel()
     }
 
 }
--- app/src/main/java/com/example/gridpics/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/gridpics/ui/details/DetailsFragment.kt
@@ -1,15 +1,20 @@
 package com.example.gridpics.ui.details
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -22,6 +27,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -36,12 +42,20 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import coil3.compose.rememberAsyncImagePainter
+import com.example.gridpics.ui.settings.SettingsViewModel
 import com.example.gridpics.ui.themes.ComposeTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class DetailsFragment : Fragment() {
 
     private var interfaceIsVisible = true
+    private val viewModel by viewModel<SettingsViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        viewModel.setTrueState()
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,6 +63,7 @@ class DetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val img = arguments?.getString("pic")!!
+        viewModel.setTrueState()
         return ComposeView(requireContext()).apply {
             setContent {
                 ShowDetails(img)
@@ -58,9 +73,42 @@ class DetailsFragment : Fragment() {
 
     @Composable
     fun ShowDetails(img: String) {
+        val isVisible = remember { mutableStateOf(true) }
+        val dynamicPadding = remember { mutableStateOf(40.dp) }
         ComposeTheme {
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter) {
-                CenterAlignedTopAppBarExample(img)
+                AnimatedVisibility(visible = isVisible.value) {
+                    @OptIn(ExperimentalMaterial3Api::class)
+                    TopAppBar(
+                        title = {
+                            Text(
+                                img,
+                                fontSize = 18.sp,
+                                maxLines = 2,
+                                modifier = Modifier.padding(0.dp, 5.dp, 0.dp, 0.dp),
+                                overflow = TextOverflow.Ellipsis,
+
+                                )
+                        },
+                        navigationIcon = {
+                            IconButton({ requireActivity().onBackPressed() }) {
+                                Icon(
+                                    Icons.Filled.ArrowBack,
+                                    contentDescription = "back",
+                                    modifier = Modifier.padding(0.dp, 10.dp, 0.dp, 0.dp)
+                                )
+                            }
+                        },
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                            actionIconContentColor = MaterialTheme.colorScheme.onPrimary
+                        )
+                    )
+                }
+            }
+            AnimatedVisibility(visible = !isVisible.value) {
+                Spacer(modifier = Modifier.fillMaxWidth().height(40.dp))
             }
             var scale by remember { mutableStateOf(1f) }
             var offset by remember { mutableStateOf(Offset(0f, 0f)) }
@@ -68,8 +116,14 @@ class DetailsFragment : Fragment() {
                 painter = rememberAsyncImagePainter(img),
                 contentDescription = null,
                 modifier = Modifier
-                    .padding(0.dp,40.dp,0.dp,0.dp)
-                    .clickable { clickOnImage() }
+                    .padding(0.dp, dynamicPadding.value, 0.dp, 0.dp)
+                    .clickable {
+                        clickOnImage()
+                        isVisible.value = !isVisible.value
+                        if (isVisible.value == true) {
+                            dynamicPadding.value = 40.dp
+                        } else dynamicPadding.value = 16.dp
+                    }
                     .pointerInput(Unit) {
                         detectTransformGestures { _, pan, zoom, _ ->
                             // Update the scale based on zoom gestures.
@@ -92,6 +146,9 @@ class DetailsFragment : Fragment() {
     }
 
     private fun clickOnImage() {
+        viewModel.observeState().observe(viewLifecycleOwner) {
+            interfaceIsVisible = it
+        }
         if (interfaceIsVisible) {
             requireActivity().window.setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -101,14 +158,14 @@ class DetailsFragment : Fragment() {
             decorView.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
                         View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            interfaceIsVisible = false
+            viewModel.setFalseState()
         } else {
             requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
             val window = requireActivity().window.decorView
             window.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_VISIBLE or
                         View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            interfaceIsVisible = true
+            viewModel.setTrueState()
         }
     }
 
@@ -116,33 +173,6 @@ class DetailsFragment : Fragment() {
     @Composable
     fun CenterAlignedTopAppBarExample(s: String) {
         ComposeTheme {
-            @OptIn(ExperimentalMaterial3Api::class)
-            TopAppBar(
-                title = {
-                    Text(
-                        s,
-                        fontSize = 18.sp,
-                        maxLines = 2,
-                        modifier = Modifier.padding(0.dp, 5.dp, 0.dp, 0.dp),
-                        overflow = TextOverflow.Ellipsis,
-
-                        )
-                },
-                navigationIcon = {
-                    IconButton({ requireActivity().onBackPressed() }) {
-                        Icon(
-                            Icons.Filled.ArrowBack,
-                            contentDescription = "back",
-                            modifier = Modifier.padding(0.dp, 10.dp, 0.dp, 0.dp)
-                        )
-                    }
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
-                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
-                    actionIconContentColor = MaterialTheme.colorScheme.onPrimary
-                )
-            )
         }
     }
 }
