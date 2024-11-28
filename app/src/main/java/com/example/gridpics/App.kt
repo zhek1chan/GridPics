f17c8e0ce3dde0970e4a904149b03b65bbdb2c2e
zhekichan <evg.ermolov@yandex.ru>
2024-11-28 15:03:55 +0500

    fullFix

--- app/src/main/java/com/example/gridpics/App.kt
+++ app/src/main/java/com/example/gridpics/App.kt
@@ -2,18 +2,15 @@ package com.example.gridpics
 
 import android.app.Application
 import android.content.Context
-import android.os.Build
 import coil3.ImageLoader
 import coil3.SingletonImageLoader
 import coil3.disk.DiskCache
 import coil3.disk.directory
 import coil3.request.CachePolicy
 import coil3.request.allowHardware
-import com.example.gridpics.data.network.getUnsafeOkHttpClient
 import com.example.gridpics.di.dataModule
 import com.example.gridpics.di.domainModule
 import com.example.gridpics.di.viewModelModule
-import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.component.KoinComponent
 import org.koin.core.context.startKoin
@@ -45,15 +42,6 @@ class App: Application(), KoinComponent, SingletonImageLoader.Factory
 				}
 				.build()
 		}
-
-		client = if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
-		{
-			getUnsafeOkHttpClient()
-		}
-		else
-		{
-			OkHttpClient()
-		}
 	}
 
 	override fun newImageLoader(context: Context): ImageLoader
@@ -66,11 +54,6 @@ class App: Application(), KoinComponent, SingletonImageLoader.Factory
 					.build()
 			}
 			.build()
-	}
-
-	companion object
-	{
-		lateinit var client: OkHttpClient
 	}
 }
 
--- app/src/main/java/com/example/gridpics/di/DataModule.kt
+++ app/src/main/java/com/example/gridpics/di/DataModule.kt
@@ -1,13 +1,15 @@
 package com.example.gridpics.di
 
 import android.content.Context
-import com.example.gridpics.App
+import android.os.Build
 import com.example.gridpics.data.network.Api
 import com.example.gridpics.data.network.NetworkClient
 import com.example.gridpics.data.network.RetrofitNetworkClient
+import com.example.gridpics.data.network.getUnsafeOkHttpClient
 import com.example.gridpics.ui.activity.MainActivity.Companion.SHARED_PREFERENCE_GRIDPICS
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -16,8 +18,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 val dataModule = module {
 	@Suppress("DEPRECATION")
 	val gson = GsonBuilder().setLenient().create()
+	val client = if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
+	{
+		getUnsafeOkHttpClient()
+	}
+	else
+	{
+		OkHttpClient()
+	}
 	single<Api> {
-		Retrofit.Builder().addConverterFactory(GsonConverterFactory.create(gson)).baseUrl("http://it-link.ru/test/").client(App.client).build().create(Api::class.java)
+		Retrofit.Builder().addConverterFactory(GsonConverterFactory.create(gson)).baseUrl("http://it-link.ru/test/").client(client).build().create(Api::class.java)
 	}
 
 	single {
